66ec16e5ea548b2fab926658c92c354a36deb56f
Μπαλάτος Δημήτριος (el21170) <el21170@mail.ntua.gr>
2024-12-17 20:21:37 +0200

    Added template mongodb code on back-end

--- back-end/main.ts
+++ back-end/main.ts
@@ -1,5 +1,17 @@
 // @ts-types='npm:@types/express'
 import express from 'npm:express';
+// SEE: https://docs.deno.com/examples/express_tutorial/
+import { MongoClient } from 'npm:mongodb';
+// SEE: https://docs.deno.com/examples/mongo/
+
+const client = new MongoClient('mongodb://localhost:27017');
+try {
+	await client.connect();
+	console.log('OK connecting to db');
+} catch (err) {
+	console.error('ERR connectint to db:', err);
+	Deno.exit(1);
+}
 
 const app = express();
 
