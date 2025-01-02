7c8368661b13d8537046ed4961954aabb4d0db61
Μπαλάτος Δημήτριος (el21170) <el21170@mail.ntua.gr>
2025-01-02 21:52:20 +0200

    Added OpenAPI - compliant routes
    Currently only GET /api/operators/, GET /api/operators/{id}, GET /

--- back-end/main.ts
+++ back-end/main.ts
@@ -1,10 +1,15 @@
 // @ts-types='npm:@types/express'
 import express from 'npm:express';
+// import cors from 'npm:cors';
+import openapi from 'npm:@wesleytodd/openapi';
 // SEE: https://docs.deno.com/examples/express_tutorial/
 import { MongoClient } from 'npm:mongodb';
 // SEE: https://docs.deno.com/examples/mongo/
 // TODO: JWT
 // SEE: https://docs.deno.com/examples/creating_and_verifying_jwt/
+
+import apiDoc from './api/api-doc.ts';
+import api from './api/router.ts';
 
 const client = new MongoClient('mongodb://localhost:27017');
 try {
@@ -16,9 +21,29 @@ try {
 }
 
 const app = express();
+const oapi = openapi(apiDoc);
 
-app.get('/', (req, res) => {
-	res.send('Welcome to Deno');
+app.use(oapi);
+app.use('/docs', oapi.swaggerui());
+
+app.use('/api', api(oapi));
+
+app.get('/', oapi.path({
+	responses: {
+		200: {
+			description: 'successful response',
+			content: {
+				schema: {
+					type: 'array',
+					items:  {
+						$ref: '#/definitions/TollOperator'
+					}
+				}
+			}
+		}
+	}
+}), (_req, res) => {
+	res.json({ id: '0', name: 'John Doe', address: 'Morge'});
 });
 
 if (import.meta.main) {
