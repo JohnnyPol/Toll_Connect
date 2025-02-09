92ff9ba5e88a15d1a0bbeacdda87e83a491a1a7f
Katerina Michou <el21079@mail.ntua.gr>
2025-02-09 18:10:06 +0200

    updatedtoll_operators  API to include change in toll operator user level

--- back-end/api/routes/db/toll_operator.ts
+++ back-end/api/routes/db/toll_operator.ts
@@ -1,6 +1,7 @@
 import { Middleware, Request, Response, Router } from 'npm:express';
 import { die, ErrorType } from '../../util.ts';
 import TollOperator from '../../../models/toll_operator.ts';
+import { UserLevel } from '../../../authentication/jwt.ts';
 
 export default function (oapi: Middleware): Router {
     const router = new Router();
@@ -11,7 +12,7 @@ export default function (oapi: Middleware): Router {
      */
     router.get('/', async (req: Request, res: Response) => {
         try {
-            const tollOperators = await TollOperator.find().lean(); 
+            const tollOperators = await TollOperator.find({userLevel:UserLevel.Operator}).lean(); 
             res.status(200).json(tollOperators);
         } catch (error) {
             console.error('Error fetching toll operators:', error);
