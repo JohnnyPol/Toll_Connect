0d316e82531c3772a43ae090be18c8fe1faaf545
Angeliki Ntalapera <el21001@mail.ntua.gr>
2025-02-11 21:16:21 +0200

    Updated import paths

--- back-end/api/router.ts
+++ back-end/api/router.ts
@@ -1,27 +1,24 @@
-import { NextFunction, Request, Response, Router } from 'express';
-import { DataItem, stringify } from '@std/csv';
+import { NextFunction, Router } from 'express';
 
-import { die, ErrorType } from '@/api/util.ts';
 import authenticate from '@/authentication/middleware.ts';
 import parser from '@/api/parser.ts';
-import { UserLevel } from '@/models/toll_operator.ts';
 
-import admin from './routes/admin.ts';
-import charges_by from './routes/charges_by.ts';
-import login from '../authentication/login.ts';
-import operators from './routes/operators.ts';
-import pass_analysis from './routes/pass_analysis.ts';
-import passes_cost from './routes/passes_cost.ts';
-import payments from './routes/payments.ts';
-import statistics from './routes/statistics.ts';
-import toll_passes from './routes/toll_station_passes.ts';
+import admin from '@/api/routes/admin.ts';
+import charges_by from '@/api/routes/charges_by.ts';
+import login from '@/authentication/login.ts';
+import operators from '@/api/routes/operators.ts';
+import pass_analysis from '@/api/routes/pass_analysis.ts';
+import passes_cost from '@/api/routes/passes_cost.ts';
+import payments from '@/api/routes/payments.ts';
+import statistics from '@/api/routes/statistics.ts';
+import toll_passes from '@/api/routes/toll_station_passes.ts';
 /* CRUD API */
-import tolls from './routes/db/tolls.ts';
-import pass from './routes/db/pass.ts';
-import payment from './routes/db/payment.ts';
-import road from './routes/db/road.ts';
-import tag from './routes/db/tag.ts';
-import toll_operators from './routes/db/toll_operators.ts';
+import tolls from '@/api/routes/db/tolls.ts';
+import pass from '@/api/routes/db/pass.ts';
+import payment from '@/api/routes/db/payment.ts';
+import road from '@/api/routes/db/road.ts';
+import tag from '@/api/routes/db/tag.ts';
+import toll_operators from '@/api/routes/db/toll_operators.ts';
 
 export default function (oapi: NextFunction): Router {
 	const router = new Router();
